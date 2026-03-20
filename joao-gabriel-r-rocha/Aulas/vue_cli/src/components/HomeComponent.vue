c1f55eba3bc208acff7d35f4c17a4f0fcb04baa4
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2026-03-19 21:21:49 -0300

    Instalando Vue

---
+++ joao-gabriel-r-rocha/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <footer id="footer">
+      <p>Bem vindo: {{ nome }} Olá Mundo do <strong>Vue.js</strong></p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: "João",
+    };
+  },
+  name: "HomeComponent",
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
