a9763255810f1ea70aa456a73c0e559a7a2cf2c4
Kyon-S2 <felipegabriel3356@gmail.com>
2026-05-07 21:46:07 -0300

    Primeira Aula Projeto Final

---
+++ Felipe Gabriel do N. Rodrigues/Projeto Final/tburguer/src/views/ConfiguracaoPedidoView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Configuração dos Pedidos</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ConfiguracaoPeiddoView',
+    }
+</script>
+
+<style scoped>
+
+</style>
