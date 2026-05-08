91f26fb74ade21b5551b488f4921a4a5f1580f6d
Lucas Palacio Mello <lucas.palacio@dinfor.uniceub>
2026-05-07 21:43:01 -0300

    inicio do projeto final

---
+++ Lucas Palacio/Projeto Final/tburguer/tburguer/src/views/ConfiguracaoPedidoView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Cofiuração do Pedidos</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ConfiguracaoPedidoView',
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Lucas Palacio/Projeto Final/tburguer/tburguer/src/views/PedidosView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Pedidos</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'PedidoView',
+
+}
+</script>
+
+<style scoped>
+
+</style>
