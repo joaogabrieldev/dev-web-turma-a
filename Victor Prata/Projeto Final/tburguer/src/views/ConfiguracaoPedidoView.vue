c10e0ba6636e0696611d6b32ab7290e8ba2988e4
Victor Rezende de Melo Prata <victorprata@dinfor.uniceub>
2026-05-07 22:07:04 -0300

    Atividade projeto final

---
+++ Victor Prata/Projeto Final/tburguer/src/views/ConfiguracaoPedidoView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Configuração do pedido</h1>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'ConfiguraçãoPedidosView'
+    }
+</script>
+<style scoped></style>
