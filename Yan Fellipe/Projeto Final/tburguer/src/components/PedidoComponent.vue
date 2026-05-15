ee15aaf774d0236373b684590633d5bb36d66894
Yan Fellipe da Silva Neri <yan.neri@dinfor.uniceub>
2026-05-14 21:39:48 -0300

    Projeto Final

---
+++ Yan Fellipe/Projeto Final/tburguer/src/components/PedidoComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <form id="pedido-form" >
+            <div>
+                <p id="nome-hamburguer-content">
+                    {{ burguer && burguer.nome ? burguer.nome : "-" }}
+
+                </p>
+                <img id="foto-content" 
+                :src="burguer && burguer.foto ? burguer.foto : ''"/>
+            </div>
+            <div class="inputs" id="form-pedido">
+                <label>Nome</label>
+                <input 
+                type="text" 
+                id="nome-cliente" 
+                name="nome-cliente" 
+                placeholder="Digite seu nome"
+                />
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+    export default {
+        name: "PedidoComponent",
+        props: {
+            burguer: null,
+        },
+    };
+</script>
+<style scoped>
+
+</style>
