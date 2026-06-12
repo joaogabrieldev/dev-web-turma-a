2e75be75a2c67cbc0343ed41f3b9145ba30cd931
Murilo Yuki <rspsukiyama21@gmail.com>
2026-06-11 21:12:10 -0300

    feat: Adição da atividade de figurinhas

--- Murilo Yuki/Projeto Final/tburguer/src/main.js
+++ Murilo Yuki/Projeto Final/tburguer/src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const app = createApp(App);
+
+app.config.globalProperties.$apiUrl = process.env.VUE_APP_API_BASE_URL
+
 createApp(App).use(router).mount('#app')
